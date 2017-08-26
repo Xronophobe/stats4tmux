6eeed9552733de07fca5bbd032c705d67bd87903
Csanad Beres <qed.csberes@gmail.com>
2017-08-26 18:14:48 +0200

    feat: default constructor, read one row files

--- src/battery.cpp
+++ src/battery.cpp
@@ -1,8 +1,23 @@
 #include "battery.hpp"
 #include <iostream>
+#include <string>
+#include <fstream>
+
+std::string Battery::getOneRowFileContent(std::string filepath)
+{
+    std::ifstream file_stream(filepath);
+    std::string content;
+    if (file_stream.is_open())
+    {
+        file_stream >> content;
+        file_stream.close();
+    }
+
+    return content;
+}
 
 int Battery::getChargePct()
 {
-    std::cout << "battery works" << std::endl;
+    std::cout << getOneRowFileContent(_energy_now) << std::endl;
     return 0;
 }
--- src/battery.hpp
+++ src/battery.hpp
@@ -5,8 +5,17 @@ class Battery
 {
 public:
 // static Battery* Instance();
-    Battery() { std::cout << "works"; }
+    Battery() :
+        _battery_path("/sys/class/power_supply/BAT0/"),
+        _energy_now(_battery_path + "energy_now"),
+        _power_now(_battery_path + "power_now")
+    {}
+    ~Battery() {}
     int getChargePct();
 private:
+    std::string _battery_path;
+    std::string _energy_now;
+    std::string _power_now;
 
+    std::string getOneRowFileContent(std::string filepath);
 };
