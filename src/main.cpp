f67fa4a684d69bd6359d1735f56e7f5b77aa9b62
Csanad Beres <qed.csberes@gmail.com>
2017-08-29 14:47:20 +0200

    test: print colored one digit representation

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
-#include <iostream>
 #include "battery.hpp"
+#include "utils.hpp"
+#include <iostream>
 
 int main()
 {
     Battery battery;
-    std::cout << battery.getChargePct() << std::endl;
     std::cout << battery.getChargingStatus() << std::endl;
+    std::cout << utils::printInOneDigit(battery.getChargePct()) << std::endl;
 }
+
